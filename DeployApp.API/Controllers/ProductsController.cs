ad7fe98c56bc2ef81a8498ed1e0c66279c2f28e0
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-07-30 15:29:34 +0300

    init

---
+++ DeployApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using DeployApp.API.Models;
+using DeployApp.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeployApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAll()
+        {
+            var products = _productService.GetAll();
+            return Ok(products);
+        }
+    }
+}
---
+++ DeployApp.API/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using DeployApp.API.Models;
+
+namespace DeployApp.API.Services
+{
+    public class ProductService
+    {
+        private readonly List<Product> _products;
+
+        public ProductService()
+        {
+            // Uygulama başlarken bu liste oluşturulur
+            _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 15000 },
+            new Product { Id = 2, Name = "Mouse", Price = 500 },
+            new Product { Id = 3, Name = "Keyboard", Price = 800 }
+        };
+        }
+
+        public IEnumerable<Product> GetAll() => _products;
+    }
+}
